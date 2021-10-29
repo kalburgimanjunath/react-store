5e881c93d7652cef05eb02026dabb9bd5c2c5d90
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-29 17:02:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qk97rs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qk97rs)
---
+++ src/store/actions/root/index.js
@@ -0,0 +1,5 @@
+import TOGGLE_VIEW from './action-types';
+
+const toggleView = () => ({
+  type: TOGGLE_VIEW,
+});
